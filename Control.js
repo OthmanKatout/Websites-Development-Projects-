c9a4a0b2b0b1da1f0563e50d71ab2d51994414ba
Othman <othmanghaleb22331@gmail.com>
2026-09-04 17:33:16 +0300

    Add the functionality of add task in js

--- Control.js
+++ Control.js
@@ -1,31 +1,58 @@
 
 const Tasks = [];
-
-function Task (Name , done){
+var cnt = 0;
+function Task (Name , done , N_O){
 
     this.Name = Name;
     this.done = done;
+    this.N_O = N_O;
+}
+
+const TaskList = document.querySelector("#First_List");
+
+function pop_it_up (){
+
+
+if(document.querySelector("#todeletelater"))return;
+
+var givemename = `<div id = "todeletelater">
+<span id = "popup">What is your task name (Up to 27 Char only 😊)</span> 
+<span id = "popup_text">
+<input type = "text" id = "input_text">
+<button id = "Confirm">✓</button>
+</span>
+</div>`;
+
+TaskList.insertAdjacentHTML('afterbegin' , givemename);
+
+const ConfirmName = document.querySelector("#Confirm");
+ConfirmName.addEventListener('click' , AddTask);
 
 }
 
-function Add (){
+function AddTask(){
 
-// here i want to ask the user what is the name of the task ?, i be sure that this name will not be as long as much to make some harm
-
-var task_name;
-const FirstList = document.querySelector("#First_List");
-var command = `" <div class = "Task">
+    const text = document.querySelector("#input_text");
+    const strtext = text.value.slice(0 , 27);
+    const eraseit = document.querySelector("#todeletelater");
+    eraseit.remove();
+    Tasks.push(new Task(strtext, false , cnt ++));
+    // call the function that render the new additions
+    
+    TaskList.innerHTML +=
+     `<div class = "Task" id = "task${cnt}">
     <div>
-    <input type = "checkbox" class = "Checkit"><label class = "Label">${task_name}</label>
+    <input type = "checkbox" class = "Checkit"><label class = "Label">${strtext}</label>
     </div>
     <div>
     <span><button class = "updateName">🛠</button></span>
     <span><button class = "deletetask">X</button></span>
     </div>
-    </div><br><br> "`
-
-FirstList.innerHTML += command;
+    </div>
+    <br><br>`;
 
 }
 
-const addbutton = document.querySelectorAll(".add");
+const AddButton = document.querySelector(".add");
+AddButton.addEventListener('click' , pop_it_up);
+
